134197098308d5e62de970f68fa3ac0207bdd4e6
AdaoJose <31747845+AdaoJose@users.noreply.github.com>
2018-01-09 11:11:23 -0200

    Update FISICO.sql

--- sql/FISICO.sql
+++ sql/FISICO.sql
@@ -1,8 +1,3 @@
--- Geração de Modelo físico
--- Sql ANSI 2003 - brModelo.
-
-
-
 CREATE TABLE Usuario (
 IdUsuario INT PRIMARY KEY,
 nomeUsuario VARCHAR(100),
@@ -11,28 +6,29 @@ dataCadastroUs VARCHAR(100),
 login VARCHAR(50),
 senha VARCHAR(50),
 ativo BOOLEAN
-)
+);
 
 CREATE TABLE setor (
 nomeSetor VARCHAR(50),
 idSetor INT PRIMARY KEY,
 dataCAdastroSet DATE,
 dataEscluSetor DATE,
-descricaoSetor VARCHAR(100),
-IdUsuario VARCHAR(100),
-idConta INT/*falha: chave estrangeira*/
-)
+descricaoSetor VARCHAR(100)
+);
 
 CREATE TABLE conta (
 idConta INT PRIMARY KEY,
 nomeConta VARCHAR(100),
 valorConta BOOLEAN,
+idSetorConta INT,
 tipoConta VARCHAR(100),
 uploadConta VARCHAR(100),
 statusConta VARCHAR(100),
 dataVencConta DATE,
+avisoAntecipadoConta DATE,
 dataValidacao DOUBLE
-)
+);
+ALTER TABLE conta ADD FOREIGN KEY(idSetorConta) REFERENCES setor (idSetor);
 
 CREATE TABLE usuarioSetor (
 IdUsuario INT,
@@ -40,6 +36,7 @@ idSetor INT,
 acesso VARCHAR(100) ,
 tipoUsuario INT,
 superiorImediato INT/*falha: chave estrangeira*//*falha: chave estrangeira*/
-)
+);
 
-ALTER TABLE setor ADD FOREIGN KEY(idConta) REFERENCES conta (idConta)
+ALTER TABLE usuarioSetor ADD FOREIGN KEY(idSetor) REFERENCES setor(idSetor);
+ALTER TABLE usuarioSetor ADD FOREIGN KEY(superiorImediato) REFERENCES Usuario (IdUsuario);
